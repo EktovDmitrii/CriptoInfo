ff7296f04456cd9502d0f99907c2f67ec3a893d2
Dmitrii <actovus@gmail.com>
2022-11-19 10:39:59 +0300

    refactoring completed

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.cryptoapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.example.cryptoapp.domain.CoinInfo
 import com.example.cryptoapp.presentation.adapter.CoinInfoAdapter
@@ -23,9 +25,11 @@ class MainActivity : AppCompatActivity() {
 
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent =
-                    CoinDetailActivity.newIntent(this@MainActivity, coinPriceInfo.fromsymbol)
-                startActivity(intent)
+                if (isOnePaneMode()) {
+launchActivity(coinPriceInfo.fromsymbol)
+                }else {
+                    launchFragment(coinPriceInfo.fromsymbol)
+                }
             }
         }
         binding.rvCoinPriceList.adapter = adapter
@@ -34,6 +38,25 @@ class MainActivity : AppCompatActivity() {
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+
+    }
+
+    private fun isOnePaneMode(): Boolean {
+        return binding.coinDetailContainer == null
+    }
+
+    private fun launchFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.coin_detail_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun launchActivity(fromSymbol: String){
+        val intent =
+        CoinDetailActivity.newIntent(this@MainActivity, fromSymbol)
+        startActivity(intent)
 
     }
 }
